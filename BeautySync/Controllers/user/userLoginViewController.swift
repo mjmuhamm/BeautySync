a05562622e11523bdafea75c41bb11ba9e7a8c72
mjmuhamm <malikmuhammad@Maliks-MBP.example.com>
2024-10-30 18:53:42 -0400

    first

---
+++ BeautySync/Controllers/user/userLoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserLoginViewController.swift
+//  BeautySync
+//
+//  Created by Malik Muhammad on 10/30/24.
+//
+
+import UIKit
+
+class userLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
